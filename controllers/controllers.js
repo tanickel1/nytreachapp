6b2375b21c4c56d806704b6aeea082b7af155fed
tanickel1 <tanickel1@gmail.com>
2018-03-19 18:33:49 -0400

    updates

---
+++ controllers/controllers.js
@@ -0,0 +1,61 @@
+var express = require('express');
+
+var router = express.Router();
+
+var Article = require('../models/article');
+
+router.get('/api/saved', function (req, res) {
+	Article.find({}, function (err, data) {
+		if (err) {
+			console.log(err);
+		} else {
+			var resultData = [];
+			data.forEach(function (article) {
+				resultData.push({
+					title: article.title,
+					url: article.url,
+					date: article.date,
+					articleID: article.articleID
+				});
+			});
+
+			res.send(resultData);
+		}
+	});
+});
+
+router.post('/api/saved', function (req) {
+	var body = req.body;
+
+	var newArticle = {
+		title: body.title,
+		url: body.url,
+		date: body.date,
+		articleID: body.articleID
+	};
+
+	var query = {articleID: bodyarticleID};
+
+	Article.findOneAndUpdate(query, newArticle, {upsert: true}, function (err) {
+		if (err) {
+			console.log(err);
+		}
+	});
+});
+
+router.delete('/api/saved/:articleID', function (req) {
+	var articleID = req.params.articleID;
+	Article.remove({articleID: articleID}, function(err) {
+		if (err) {
+			console.log(err);
+		}
+	});
+});
+
+
+router.use('*', function (req, res) {
+	res.sendFile('index.html');
+});
+
+module.exports = router;
+
