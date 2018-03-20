9ca5847f0992a0c7728b3fb05795f310556531fc
tanickel1 <tanickel1@gmail.com>
2018-03-20 16:52:58 -0400

    creating more initial files. adding bootswatch. and making initial html file for testing

---
+++ models/article.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoos');
+
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema({
+    title: {
+        type: String, 
+        trim: true, 
+        required: true
+    },
+    date: {
+        type: String, 
+        trim: true, 
+        required: true
+    }, 
+    url: {
+        type: String, 
+        trim: true,
+        required: true
+    },
+    articleID: {
+        type: String, 
+        trim: true,
+        required: true
+    }
+});
+
+var Article = mongoose.model('Article', articleSchema);
+
+module.exports = Article; 
